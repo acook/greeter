50202d3be75d734cdf65975973ba793b66ea2787
Anthony Cook <anthonymichaelcook@gmail.com>
2011-09-07 14:20:59 -0400

    Created Customer class and abstracted greeting and farewell functionality into a module

--- greeter.rb
+++ greeter.rb
@@ -1,12 +1,45 @@
 #!/usr/bin/env ruby
 # This script will greet people and stuff
 
-class Greeter
-  def hello name
-    puts "Hello, #{name}!"
+module GreetingAndFarewell
+  def name
+    @name ||= "person who will give me money for some product or service"
+  end
+
+  def name= new_name
+    @name = new_name
+  end
+
+  def hello
+    puts "Hello, #{@name}!"
+  end
+
+  def farewell
+    puts "Farewell, #{@name}!"
   end
 end
 
-my_greeter = Greeter.new
-my_greeter.hello ARGV.first
+class Greeter
+  include GreetingAndFarewell
+
+  def initialize new_name = nil
+    @name = new_name
+  end
+end
+
+class Customer
+  include GreetingAndFarewell
+
+  def name
+    @name ||= "guy who will provide me with a good or service in exchange for monetary compensation"
+  end
+end
+
+#my_greeter = Greeter.new ARGV.first
+#name = ARGV.first
+#my_greeter.hello
+#my_greeter.farewell
+
+# call security
+# farewell
 
