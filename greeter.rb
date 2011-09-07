c2a8a3503483385ebe5f43cdf53ed6ca4aba2916
Anthony Cook <anthonymichaelcook@gmail.com>
2011-09-07 13:30:33 -0400

    Made the greeter class actually output a greeting

--- greeter.rb
+++ greeter.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
 # This script will greet people and stuff
 
+class Greeter
+  def hello name
+    puts "Hello, #{name}!"
+  end
+end
+
+my_greeter = Greeter.new
+my_greeter.hello ARGV.first
 
